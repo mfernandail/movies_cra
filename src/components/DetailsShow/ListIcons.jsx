28a4dd69ca3131f3308184d0dc31dcd8d0e04eb0
MariaFernanda <maria.ibanez.leiva@gmail.com>
2021-10-26 00:15:29 -0300

    fix details icons

--- src/components/DetailsShow/ListIcons.jsx
+++ src/components/DetailsShow/ListIcons.jsx
@@ -4,9 +4,11 @@ import './ListIcons.css';
 export const ListIcons = ({icon, data, description}) => {
   return (
     <>
-      <img className="movie-section__icon" src={icon} alt={description} />
-      <span className="movie-section__desc">{description}:</span>
-      <span className="movie-section__data">{data}</span>
+      <span> 
+        <img className="movie-section__icon" src={icon} alt={description} />
+        <span className="movie-section__desc">{description}:</span>
+        <span className="movie-section__data">{data}</span>
+      </span>
     </>
   )
 }
